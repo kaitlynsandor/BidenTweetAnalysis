8818056816144990ef5393cfa234f56ac02f3642
kaitlynsandor <kaitlyn.sandor@gmail.com>
2022-11-07 21:39:12 -0500

    changed some tasks

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
