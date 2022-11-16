1e63530ca68f57011cc6f103e18f4048f35b9c8a
Zoe Hsu <zoehsu@Zoes-MBP-3.wireless.yale.internal>
2022-11-15 19:05:55 -0500

    added the new biden csv

--- data_cleaning/clean.py
+++ data_cleaning/clean.py
@@ -4,38 +4,38 @@ import datetime
 import re
 
 # convert to dataframes
-biden_tweets_df = pd.read_csv('JoeBidenTweets.csv')
-polling_df = pd.read_csv('polling_data.csv')
+biden_tweets_df = pd.read_csv('data_cleaning/tweets_of_joe_biden.csv')
+polling_df = pd.read_csv('data_cleaning/polling_data.csv')
 
 # drop unnecessary columns
-biden_tweets_df = biden_tweets_df.drop(['url'], axis = 1)
+biden_tweets_df = biden_tweets_df.drop(['tweet_url'], axis = 1)
 polling_df = polling_df.drop(['subgroup', 'enddate', 'url', 'president'], axis = 1)
 
 # split the date and the time in biden_tweets_df
-new = biden_tweets_df["timestamp"].str.split(" ", n = 1, expand = True)
-biden_tweets_df["timestamp"]= new[0]
+new = biden_tweets_df["tweet_date"].str.split(" ", n = 1, expand = True)
+biden_tweets_df["tweet_date"]= new[0]
 
 # convert timestamps
 def timestamps():
     i = 0
-    for t in biden_tweets_df["timestamp"]:
-        biden_tweets_df["timestamp"].loc[i] = datetime.datetime.strptime(t, '%Y-%m-%d').strftime("%m/%d/%Y")
+    for t in biden_tweets_df["tweet_date"]:
+        biden_tweets_df["tweet_date"].loc[i] = datetime.datetime.strptime(t, '%Y-%m-%d').strftime("%m/%d/%Y")
         i += 1
 
 # delete links in tweets
 def delete_urls():
     i = 0
-    for tweet in biden_tweets_df["tweet"]:
-        biden_tweets_df["tweet"].loc[i] = re.sub(r'http:\S+', '', tweet)
+    for tweet in biden_tweets_df["tweet_content"]:
+        biden_tweets_df["tweet_content"].loc[i] = re.sub(r'http:\S+', '', tweet)
         i += 1
 
 #testing
 timestamps()
 delete_urls()
-print(biden_tweets_df["timestamp"][0])
-print(biden_tweets_df["timestamp"][88])
-print(biden_tweets_df["timestamp"][100])
-print(biden_tweets_df["tweet"][100])
+print(biden_tweets_df["tweet_date"][0])
+print(biden_tweets_df["tweet_date"][88])
+print(biden_tweets_df["tweet_date"][100])
+print(biden_tweets_df["tweet_content"][100])
 
 # export csvs
 biden_tweets_df.to_csv(r'BidenTweetsFinal.csv')
